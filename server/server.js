6deab31374389d5d5bf0ad666af7c0e67674cf18
Adam Pugh <adampugh@zoho.com>
2017-07-12 20:22:13 +0100

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,17 +1,29 @@
 const path = require("path");
-const publicPath = path.join(__dirname, "../public");
-
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
+
+const publicPath = path.join(__dirname, "../public");
 const app = express();
 const port = process.env.PORT || 3000;
-
+var server = http.createServer(app);
+//returns web socket server - how we will communicate
+// between server and client
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.get("/", (req, res) => {
-    res.render("index.html");
+//alllows you to register an event listener
+//connection allows you to do something when a client connects with the server
+io.on("connection", (socket) => {
+    console.log("new user connected");
+
+    socket.on("disconnect", (socket) => {
+        console.log("User was disconnected");
+    });
 });
 
-app.listen(port, () => {
+
+server.listen(port, () => {
     console.log(`Server started on ${port}`);
 });
